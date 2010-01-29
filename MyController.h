fb462481be57b3fdf818bbb4afd40a66b0fe739a
Jan <github@geheimwerk.de>
2010-01-29 15:42:55 +0100

    Initial commit

---
+++ MyController.h
@@ -0,0 +1,20 @@
+//
+//  MyController.h
+//  Stepper Test
+//
+//  Created by Jan on 29.01.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MyController : NSObject {
+
+	int _editValue;
+	
+}
+
+@property (nonatomic, readwrite) int editValue;
+
+@end
---
+++ MyController.m
@@ -0,0 +1,16 @@
+//
+//  MyController.m
+//  Stepper Test
+//
+//  Created by Jan on 29.01.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyController.h"
+
+
+@implementation MyController
+
+@synthesize editValue = _editValue;
+
+@end
